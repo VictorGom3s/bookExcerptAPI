d2f84ce93b4342f26b45eb4c6aa7841fd232d8ad
Victor Sampaio <vsampaio@pm.me>
2020-03-12 19:59:45 -0300

    Adding CORS

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 /* Getting the routes */
 const textsRoutes = require("./src/routes/texts.routes");
